5d8b451ec8f0067bd0ceda169617ec1f4e56f1d6
Esteban-can <125224817+Esteban-can@users.noreply.github.com>
2023-05-04 00:17:54 -0600

    sexto_push

--- Proyecto.cpp
+++ Proyecto.cpp
@@ -1,162 +1,39 @@
 #include <iostream>
+#include <string.h>
+
+
 using namespace std;
-const char *nombre_archivo = "archivo.dat"; 
-struct Palabra 
-{
-	int op;
-	char nomb[50];
-	char def[150];
-	char tra[150];
+
+const char *nombre_archivo = "archivo.dat";
+
+//De momento la cantidad de lineas se define aca
+int lineas_total = 500;
+string *lineas_temp = new string[lineas_total];
+
+
+struct palabra_2{
+	char nombre_2[50];
+	char traduccion_2[50];
+	char significado_2[150];
 };
 
-void leer();
-void crear();
-void Actualizar();
-void Borrar();
-void Menu();
+palabra_2 pal_2;
 
-main()
-{
-	Menu();	
-   
-    scanf("");
-    return(0);
-}
-void Menu()
-{
-	int op;
-	do 
-	{
-		cout<<"Menu:"<<endl;
-		cout<<"1. Leer"<<endl;
-		cout<<"2. Crear"<<endl;
-		cout<<"3. Actualizar"<<endl;
-		cout<<"4. Borrar"<<endl;
-		cout<<"5. Salir"<<endl;
-		cin>>op;
-			switch(op)
-			{
-			case 1: leer();
-			system("pause");
-			break;
-			
-			case 2: crear();
-			system("pause");
-			break;
-			
-			case 3:Actualizar();
-			system("pause");
-			break;			    
-		
-	    	case 4: Borrar();
-	    	system("pause");
-	    	break;
-		
-	}
-	system("cls");
+void comparar(string texto_2, int contador){
+	FILE *archivo = fopen(archivo.dat, "rb");
+	int id=1, encontrado=0;
 	
-	}while(op!=5);
-}
-void leer()
-{
-	system("cls");	
-	FILE* archivo = fopen(nombre_archivo,"rb ");
-	if (!archivo)
-	{
-	archivo = fopen(nombre_archivo,"w+b ");	
-	}
-	Palabra palabra;
-	int id=0;
-	fread(&palabra,sizeof(Palabra),1,archivo);
-	cout<<"___________________________________________________"<<endl;
-	cout<<"id"<<"|"<<"Palabra"<<"|"<<"Definicion"<<"|"<<"Traduccion"<<"|"<<endl;
-	do
-	{
-		cout<<id<<" | "<<palabra.nomb<<" | "<<palabra.def<<" | "<<palabra.tra<<endl;
-		fread(&palabra,sizeof(Palabra),1,archivo);
-	id+=1;	
-	}while (feof(archivo)==0);
-	fclose(archivo);
-}
-
-void crear()
-
-{
-FILE* archivo = fopen(nombre_archivo,"a+b");
-char res;
-Palabra palabra;
-do
-{
-	fflush(stdin);
-	cout<<"Ingrese Palabra: ";
-	cin.getline(palabra.nomb,150);
-	cout<<"Ingrese Traduccion: ";
-	cin.getline(palabra.tra,150);
-	cout<<"Ingrese Definicion: ";
-	cin.getline(palabra.def,150);
-	
-	
-	fwrite(&palabra,sizeof(Palabra),1,archivo);
-	
-	cout <<"Desea ingresar otra palabra (s/n):";
-	cin>>res;
-}while(res == 's'|| res == 'S');
-fclose(archivo);
-leer();
-	
-	system ("pause");
-} 
-
-void Borrar()
-{
-	
-	const char *nombre_archivo_temp = "archivo_temp.dat";
-	system("cls");	
-	FILE* archivo_temp = fopen(nombre_archivo_temp,"w+b");
-	FILE* archivo = fopen(nombre_archivo,"rb ");
-	Palabra palabra;
-	int id=0,id_n=0;
-	cout<<"Ingrese el Numero a eliminar:";
-	cin>>id;
-	while(fread(&palabra,sizeof(Palabra),1,archivo))
-	{
-		if (id_n!=id)
-		{ 
-				fwrite(&palabra,sizeof(Palabra),1,archivo_temp);
+	fread(&pal_2,sizeof(palabra_2),1,archivo);
+	do{		
+		if (texto_2 == pal_2.nombre_2){
+			lineas_temp[contador] = lineas_temp[contador] + ' ' + pal_2.traduccion_2;
+			encontrado = 1;
 		}
-		id_n++;
-	} 
+		fread(&pal_2,sizeof(palabra_2),1,archivo);
+		id += 1;
+	}while(feof(archivo)==0);
 	fclose(archivo);
-	fclose(archivo_temp);
-	 archivo_temp = fopen(nombre_archivo_temp,"rb");
-	 archivo = fopen(nombre_archivo,"wb ");
-	 	while(fread(&palabra,sizeof(Palabra),1,archivo_temp))
-	 	{
-	 	  	fwrite(&palabra,sizeof(Palabra),1,archivo);
-		 }
-		 fclose(archivo);
-	fclose(archivo_temp);
-leer();	
-}
-void Actualizar()
-{
-FILE* archivo = fopen(nombre_archivo,"r+b"); 
-Palabra palabra;
-	int id=0;
-	cout<<"Ingrese el ID que desea Modificar:";
-	cin>>id;
-	fseek(archivo,id * sizeof(Palabra),SEEK_SET); 
-	cout<<"Ingrese Palabra: ";
-	cin.get(palabra.nomb,150);
-	cout<<"Ingrese Traduccion: ";
-	cin.getline(palabra.tra,150);
-	cout<<"Ingrese Definicion: ";
-	cin.getline(palabra.def,150);
-		
-	fwrite(&palabra,sizeof(Palabra),1,archivo);
-	cout <<"Desea ingresar otro estudiante (s/n):";
-	
-
-fclose(archivo);
-leer();	
+	if (encontrado != 1){
+		lineas_temp[contador] = lineas_temp[contador] + ' ' + texto_2;
+	}
 }
