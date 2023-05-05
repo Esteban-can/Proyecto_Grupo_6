2f8585cc691c4a8110e61dcfde9b3b7dc55f5393
1290-22-3226 <124806964+1290-22-3226@users.noreply.github.com>
2023-05-04 23:36:34 -0600

    Cuarto Push Fase_2

--- Fase_2.cpp
+++ Fase_2.cpp
@@ -20,14 +20,14 @@ string nombre_2tem, traduccion_2tem, significado2_tem;
 int borra_ese;
 
 void crear_archivo(){
-	FILE *archivo = fopen((palabras.txt, "r+b");
-	if (!archivo){
-		archivo = fopen((palabras.txt,"w+b");	
+	FILE *palabra = fopen((nombre_archivo, "r+b");
+	if (!palabra){
+		palabra = fopen((nombre_archivo,"w+b");	
 	}
 }
 void leer(int buscar){
 	//Si buscar es igual a "1" la funci�n buscara un item; Si buscar es distinto a "1" mostrara todos los items
-	FILE *archivo = fopen(palabras.txt, "rb");
+	FILE *archivo = fopen(nombre_archivo, "rb");
 	//palabra pal;
 	
 	int id=1, bus;
@@ -56,7 +56,7 @@ void leer(int buscar){
 
 //Guarda los valores a remplazar en una variable
 void ayuda_remplazar(int help){
-	FILE *archivo = fopen(palabrastxt, "rb");
+	FILE *archivo = fopen(nombre_archivo, "rb");
 	//palabra pal;
 	int id = 1;
 	fread(&pal,sizeof(palabra),1,archivo);
@@ -83,12 +83,50 @@ void ingreso(int cremo){
 		cout<<"¡--Ingreso de valores--!"<<endl<<endl;
 	}else{
 		fclose(archivo);
-		FILE* archivo = fopen(nombre_archivo,"r+b");
+		FILE* palabra = fopen(nombre_archivo,"r+b");
 		int id;
 		//Si no se quiere modificar un campo colocar "0"
 		cout<<"Ingrese el ID que desea Modificar: ";
 		cin>>id;
-		fseek(archivo,(id-1) * sizeof(archivo),SEEK_SET);
+		fseek(palabra,(id-1) * sizeof(palabra),SEEK_SET);
 		ayuda_remplazar(id);
 		cin.ignore();
+	}
+	
+	//Si es verdadero se "borrara"
+	if(borra_ese == 1){
+		strcpy(pal.nombre, "--");
+		strcpy(pal.traduccion, "--");
+		strcpy(pal.significado, "--");
+	}else{	
+		
+		//Si no es necesario remplazar un valor en especifico se ingresa 0; Al ingresar 0, este valor se remplaza por la variable temporal (Vease ayuda_remplazar()). 
+		cout<<"Ingrese el nombre: ";
+		getline(cin,str_nombre);
+		if(str_nombre == "0"){
+			strcpy(pal.nombre, nombre_tem.c_str());
+		}else{
+			strcpy(pal.nombre, str_nombre.c_str());
+		}
+		
+		cout<<"Ingrese la traduccion: ";
+		getline(cin,str_traduccion);
+		if(str_traduccion == "0"){
+			strcpy(pal.traduccion, traduccion_tem.c_str());
+		}else{
+			strcpy(pal.traduccion, str_traduccion.c_str());
+		}
+		
+		
+		cout<<"Ingrese el significado: ";
+		getline(cin,str_significado);
+		if(str_significado == "0"){
+			strcpy(pal.significado, significado_tem.c_str());
+		}else{
+			strcpy(pal.significado, str_significado.c_str());
+		}
+		
+		//cin.ignore();
+		
+		//leer(0);
 	}
